b33d00757830f4f7b9b005ec5b18c3ac68fc294a
Andrew <andrew.fleer@gmail.com>
2021-05-10 15:54:39 -0500

    Update SameDigits.java

--- InterviewQuestions/src/SameDigits.java
+++ InterviewQuestions/src/SameDigits.java
@@ -6,24 +6,18 @@ public class SameDigits {
         System.out.println("Test case 1 => " + sameDigits(1)); // true
         System.out.println("Test case 10 => " + sameDigits(10)); // true
         System.out.println("Test case 251894 => " + sameDigits(251894)); // true
-        System.out.println("Test case 251985 => " + sameDigits(251895)); // false
+        System.out.println("Test case 251895 => " + sameDigits(251895)); // false
 
     }
 
     private static boolean sameDigits(long i) {
 
         // build a hashset of unique digits
-        HashSet<Character> originalSet = new HashSet<Character>();
-        for (char c : Long.toString(i).toCharArray()) {
-            originalSet.add(c);
-        }
+        HashSet<Character> originalSet = createHashSet(i);
 
         // build a hashset of cubed digits
         long iCubed = i * i * i;
-        HashSet<Character> newSet = new HashSet<Character>();
-        for (char c : Long.toString(iCubed).toCharArray()) {
-            newSet.add(c);
-        }
+        HashSet<Character> newSet = createHashSet(iCubed);
 
         // If the sets aren't the same size, they obviously don't have the same digits
         if (originalSet.size() != newSet.size()) {
@@ -31,6 +25,15 @@ public class SameDigits {
         }
 
         return newSet.containsAll(originalSet);
+    }
+
+    private static HashSet<Character> createHashSet(long value) {
+        HashSet<Character> returnSet = new HashSet<Character>();
+        for (char c : Long.toString(value).toCharArray()) {
+            returnSet.add(c);
+        }
+
+        return returnSet;
     }
 
 }
